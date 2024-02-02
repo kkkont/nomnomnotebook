3dd6ed89f5aaefdcc6752a48f46bd8d291d60c68
kkkont <116272343+kkkont@users.noreply.github.com>
2024-02-02 22:59:23 +0200

    Footer on all recipes page

--- front/src/views/AllRecipesView.vue
+++ front/src/views/AllRecipesView.vue
@@ -6,18 +6,21 @@
         <AllRecipesComponent></AllRecipesComponent>
       </div>
     </div>
+    <FooterComponent></FooterComponent>
   </div>
 </template>
 
 <script>
 import AllRecipesComponent from "@/components/AllRecipesComponent.vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
 
 export default {
   name: "AllRecipesView",
   components: {
     HeaderComponent,
     AllRecipesComponent,
+    FooterComponent,
   },
 };
 </script>
